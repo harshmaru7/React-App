73825e66e2aeeebdec507fdbfa9652ba8c7caf18
harshmaru7 <harsh.171it215@nitk.edu.in>
2021-10-08 10:36:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      item : "",
+      task : [],
+    }
+  };
+  onChangeValue = event =>{
+    this.setState({item : event.target.value});
+  };
+  addTask = e =>{
+    e.preventDefault()
+    this.setState({
+      task: [ this.state.item,...this.state.task],
+      item: ''
+    })
+  }; 
+   deleteTask = (index) => {
+    const newTask = [...this.state.task];
+    newTask.splice(index, 1);
+
+    this.setState(state => ({
+        task: newTask
+    }));}
+  
+  clearList = () =>{
+    this.setState({task: []});
+  }
+  render(){
+    return (
+
+        <div className="App">
+        <h1>To Do App! Increase your productivity !</h1>
+        <input type="text" value={this.state.value} onChange={this.onChangeValue}/>
+        <button type="button" onClick={this.addTask}>Add Task</button>
+        {/* <button type="button" onClick={()=>this.deleteTask(index)}>Remove</button> */}
+        <button type="button" onClick={this.clearList}>New List</button>
+        {/* <div id="div2">{this.state.task.map((person, index) => (
+        <p key={index}>{person}</p>))}
+        </div> */}
+        <div className="App">
+        <ul>
+                    {this.state.task.map((person, index) => (
+                        <li key={index}>
+                            <span>{person}  </span>
+                            <input type="button" value="Done" onClick={() => this.deleteTask(index)} />
+
+                        </li>
+                    ))}
+                </ul>
+            </div>
     </div>
   );
+  }
 }
 
-export default App;
+// export default App;
